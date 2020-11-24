07e40dcc60880f8b3f7c31c07fefaaf137dc9d18
kaneli <erjasalminen@live.com>
2020-11-24 19:21:09 +0200

    eka push

---
+++ Assets/scipts/hahmoohjain.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class hahmoohjain : MonoBehaviour
+{
+    public float juoksunopeus = 3f;
+    public float hiirenNopeus = 3f;
+
+    private  float vertikaalinenPyörintä = 0f;
+
+    private float horisontaalinenPyörintä = 0f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horisontaalinenPyörintä += Input.GetAxis("Mouse X") * hiirenNopeus;
+         vertikaalinenPyörintä += Input.GetAxis("Mouse Y") * hiirenNopeus;
+
+        Camera.main.transform.localRotation = Quaternion.Euler(vertikaalinenPyörintä,horisontaalinenPyörintä,0);
+        
+    }
+}
